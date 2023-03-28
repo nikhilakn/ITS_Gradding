e38fb10a40628719069e52d2b9a9ffba3a894794
KN Nikhila <knnikhila@KNs-MacBook-Pro.local>
2023-03-28 15:02:39 +0800

    Exception handling added

--- q1_model/approachMatch/grade.py
+++ q1_model/approachMatch/grade.py
@@ -7,26 +7,27 @@ import pickle
 
 class getGrade:
     def gradeIcorrect(self,fp1,fp2,t):
-        fp1="../../../Exp/DataSet/APR_Data_set/Python/data/question_1/code/Incorrect/wrong_1_008.py" #incorrect solution
-        fp2="../../../Exp/DataSet/APR_Data_set/Python/data/question_1/code/Correct/correct_1_005.py" #reference solution
-        test=t #number of test case passed
-        fn=0  #Assume there is no flase negative
-        aproach=ApproachMatch()
-        ssm=aproach.findApproach(fp1,fp2)#Get similarity score using SSM method
-        input_data={"testcases": test,"ssm": ssm,"fn": fn}
-        df = pd.DataFrame(input_data)
-        regr_rf=pickle.load(open("q1_gm.sav", 'rb'))#load pre-trained grading model for question1
-        pred_g=regr_rf.predict(df.values)
-        #print(pred_g)
-        return pred_g
+        try:
+            test=t #number of test case passed
+            fn=0  #Assume there is no flase negative
+            aproach=ApproachMatch()
+            ssm=aproach.findApproach(fp1,fp2)#Get similarity score using SSM method
+            input_data={"testcases": test,"ssm": ssm,"fn": fn}
+            df = pd.DataFrame(input_data)
+            regr_rf=pickle.load(open("q1_gm.sav", 'rb'))#load pre-trained grading model for question1
+            pred_g=regr_rf.predict(df.values)
+            #print(pred_g)
+            return pred_g
+        except FileNotFoundError as e:
+            print(str(e))
+            exit()
         
 
 g=getGrade()
 
-fp1="../../../Exp/DataSet/APR_Data_set/Python/data/question_1/code/Incorrect/wrong_1_008.py" #incorrect solution
+fp1="../../../Exp/DataSet/APR_Data_set/Python/data/fd/question_1/code/Incorrect/wrong_1_008.py" #incorrect solution
 fp2="../../../Exp/DataSet/APR_Data_set/Python/data/question_1/code/Correct/correct_1_005.py" #reference solution
 t=9
-
 grade=g.gradeIcorrect(fp1,fp2,t)
 print(grade)
 
