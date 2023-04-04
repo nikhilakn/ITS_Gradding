c29c5e283a15dc8931492890f45595a970dcb9b3
KN Nikhila <knnikhila@r-216-96-25-172.comp.nus.edu.sg>
2023-04-04 15:17:45 +0800

    models for question 3,4,5 added

---
+++ models/approachMatch/grade_q4.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import pandas as pd
+from am import ApproachMatch
+import pickle
+
+
+class getGrade:
+    def gradeIcorrect(self,fp1,fp2,t):
+        try:
+            test=t #number of test case passed
+            fn=0  #Assume there is no flase negative
+            aproach=ApproachMatch()
+            ssm=aproach.findApproach(fp1,fp2)#Get similarity score using SSM method
+            input_data={"testcases": test,"ssm": ssm,"fn": fn}
+            df = pd.DataFrame(input_data)
+            regr_rf=pickle.load(open("q4_gm.sav", 'rb'))#load pre-trained grading model for question1
+            pred_g=regr_rf.predict(df.values)
+            #print(pred_g)
+            return pred_g
+        except FileNotFoundError as e:
+            print(str(e))
+            exit()
+        
+
+g=getGrade()
+
+fp1="../../../Exp/DataSet/APR_Data_set/Python/data/question_4/code/Incorrect/wrong_4_008.py" #incorrect solution
+fp2="../../../Exp/DataSet/APR_Data_set/Python/data/question_4/code/Correct/correct_4_001.py" #reference solution
+t=4
+grade=g.gradeIcorrect(fp1,fp2,t)
+print(grade)
+
+
+
+
+
+
